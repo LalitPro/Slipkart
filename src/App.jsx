61e7ac58ceb6737cd3a0328150832d288e1e27ba
LalitPro <140280rakesh@gmail.com>
2024-10-29 15:04:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,18 @@ function App() {
          <Route
           path="/Cart/"
           element={
-            <>
-              <ControlStrip />
-              <ProductDetail />
-            </>
+          
+                 <div className="flex flex-col items-center justify-center h-screen p-20 text-center bg-gray-100">
+      <h1 className="m-5 font-semibold text-rose-500 text-9xl">
+        Work in Progress
+      </h1>
+      <Link
+        to="/"
+        className="p-5 m-5 bg-indigo-700 border shadow-xl hover:bg-slate-200 hover:text-indigo-700 rounded-3xl text-7xl text-slate-200"
+      >
+        Go Back
+      </Link>
+    </div>
           }
         ></Route>
         <Route path="*" element={<NoFound />}></Route>
