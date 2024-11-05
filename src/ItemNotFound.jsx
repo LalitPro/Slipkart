8d46f46e309a40477436363a31d8973c30728f49
Lalit Yadav <140280rakesh@google.com>
2024-11-05 19:37:37 +0530

    n

--- src/ItemNotFound.jsx
+++ src/ItemNotFound.jsx
@@ -5,7 +5,7 @@ function ItemNotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen p-2 text-center bg-gray-100 sm:p-5 md:p-10">
       <img
-        src="PageNotFound.png"
+        src="/PageNotFound.png"
         className="w-96 md:w-1/2 rounded-full shadow-md"
       />
       <h1 className="m-2 text-3xl font-semibold sm:m-5 text-rose-500 sm:text-5xl md:text-7xl">
